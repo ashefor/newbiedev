069459da09fab6fa3387121cb4cd1a0b2aa61cb3
Shredder <mashefor@gmail.com>
2019-09-24 01:12:02 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,24 +11,9 @@ import { User } from './components/user/user';
 export class AppComponent {
   title = 'newbie';
   @ViewChild('navbar', {static: false}) navBar: ElementRef<HTMLElement>
-  // public currentUser;
   currentUser;
-  // thisUser: User
   constructor(private authservice: AuthService, private router: Router) {
-    // this.currentUser = localStorage.getItem('currentUser')? JSON.parse(localStorage.getItem('currentUser')): '';
     this.authservice.currentUser.subscribe(data => this.currentUser = data)
-  }
-
-  // get isLoggedIn() {
-  //   return this.authservice.isLoggedIn
-  // }
-
-  get userName() {
-    if (this.authservice.currentUSer) {
-      // return this.authservice.currentUSer.userName
-      return this.authservice.currentUserValue.username
-    }
-    return ''
   }
   logOut() {
     this.authservice.logOut();
