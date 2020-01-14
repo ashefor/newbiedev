a1d18517cc7e2f70ce3beda851f74ecc4e0b1ee3
Shredder <mashefor@gmail.com>
2020-01-14 11:39:27 +0100

    fix(auth guard):
    prevent loggedin user from accessing login page

--- src/app/components/user/user.routes.ts
+++ src/app/components/user/user.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ResetPwdComponent } from './reset-pwd/reset-pwd.component';
 import { RegisterComponent } from './register/register.component';
+import { isLoggedInGuard } from 'src/app/guards/isLoggedIn.guard';
 
 export const userRoutes: Routes = [
-    {path: 'register', component: RegisterComponent},
-    { path: 'login', component: LoginComponent },
+    {path: 'register', component: RegisterComponent, canActivate: [isLoggedInGuard] },
+    { path: 'login', component: LoginComponent, canActivate: [isLoggedInGuard] },
     { path: 'reset-password', component: ResetPwdComponent }
-]
+];
--- src/app/guards/isLoggedIn.guard.ts
+++ src/app/guards/isLoggedIn.guard.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
+// tslint:disable-next-line: class-name
 export class isLoggedInGuard implements CanActivate {
-    
+
     constructor(private authservice: AuthService, private router: Router) { }
-    
+
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if(localStorage.getItem('currentUser')){
-            return true;
-        }else{
-            this.router.navigate(['auth/login'], {queryParams: {returnUrl: state.url}})
+        if (localStorage.getItem('currentUser')) {
+            this.router.navigate(['/posts']);
             return false;
+        } else {
+            return true;
         }
     }
 }
